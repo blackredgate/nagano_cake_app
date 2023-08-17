a3397666c8dca40e923a27e6988964b464820979
blackredgate <138416861+blackredgate@users.noreply.github.com>
2023-08-17 09:58:27 +0000

    [Add]Bootstrap、top/aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  get 'homes/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
