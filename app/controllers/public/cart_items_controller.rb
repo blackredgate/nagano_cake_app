4ae38fe3b5931f595f261acaebca78ff37d02a2c
blackredgate <138416861+blackredgate@users.noreply.github.com>
2023-09-08 13:39:14 +0000

    [Add]カート数量変更機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,25 +3,43 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @total = 0
   end
 
   def create
     @item = Item.find(cart_item_params[:item_id])
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    @cart_item.save
-    redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def update
-
-  end
-
-  def destroy
-
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      flash[:notice] = "更新に成功しました"
+      redirect_to cart_items_path
+    else
+      render :index
+    end
   end
 
   def destroy_all
+    @cart_item = current_customer.cart_items
+    @cart_item.destroy_all
+    redirect_to cart_items_path
+  end
 
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,30 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   namespace :admin do
     get 'top' => 'homes#top'
     resources :items
     resources :customers, only:[:index, :show, :edit, :update]
   end
-  
+
   scope module: :public do
     resources :items, only:[:index, :show]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
     resources :cart_items, only:[:index, :update, :destroy, :create]
   end
-  
-  delete '/cart_items/destroy_all' => 'public/cart_items#destroy_all'
-  
+
   get '/customers' => 'public/customers#show'
   get '/customers/information/edit' => 'public/customers#edit'
   patch '/customers/information' => 'public/customers#update'
   get '/customers/unsubscribe' => 'public/customers#unsubscribe'
   patch '/customers/withdraw' => 'public/customers#withdraw'
-  
+
   root to: 'homes#top'
   get '/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
